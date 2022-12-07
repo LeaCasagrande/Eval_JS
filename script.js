bb8ba74ab3ae9cc1341e73945669e95aa5f34ed0
LeaCasagrande <lea.casagrande27@gmail.com>
2022-12-07 18:57:21 +0100

    modif

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 let space = " ";
 let tabUser = [];
+let list = document.getElementById('listUsers');
 
-function getInfo(){
+function add(){
+    //1. et 2. Créer et afficher des utilisateurs 
     let nom = document.getElementById('nom').value;
     let prenom = document.getElementById('prenom').value;
     let age = document.getElementById('age').value;
 
-    let list = document.getElementById('listUsers');
     let eachUser = document.createElement('li');
-    eachUser.setAttribute('class','suppr')
     eachUser.textContent = nom + space + prenom + space + age;
     list.appendChild(eachUser);
-    tabUser.push("Nom : "+nom+"Prénom : "+prenom+"Age : "+age);
-    console.log(tabUser)
+    
+    //3. Enregistrer les utilisateurs affichés dans une liste
+    tabUser.push("Nom : "+nom+", Prénom : "+prenom+", Age : "+age);
+    console.log(tabUser);
+
+    //8. Ajouter un bouton sur chaque ligne 
+    let btn = document.createElement('button');
+    btn.setAttribute('class','suppr');
+    btn.textContent = 'X';
+    eachUser.appendChild(btn);
 }
 
-function supprimer(){
-    let suppr = document.getElementsByClassName('suppr');
-    for(let i = 0; i<suppr.length; i++){
-        suppr[i].remove();
+//4. Supprimer tous les utilisateurs
+function removeAll(){
+    while(tabUser.length>0){
+        list.lastChild.remove();
+        tabUser.pop();
+    }
+}
+
+//5. Trier la liste en fonction du nom 
+function sortName(){
+    tabUser.sort();
+    console.log(tabUser);
+}
+
+//6. Afficher la liste triée
+
+//7. Trier par prénom et âge
+
+//9. Créer un événement sur le bouton
+let supprBtn = document.getElementsByClassName('suppr');
+console.log(supprBtn);
+
+for(let i=0; i<supprBtn.length; i++){
+    supprBtn[i].onclick = function(){
+        this.target.parentNode.remove();
     }
 }
